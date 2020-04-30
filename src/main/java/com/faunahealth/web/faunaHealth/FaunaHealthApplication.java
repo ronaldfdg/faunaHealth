2654433ae876a5d3820faa95d8e4d1496b328e79
ronaldfdg <ronaldfdg@gmail.com>
2020-04-29 21:17:31 -0500

    First commit

---
+++ src/main/java/com/faunahealth/web/faunaHealth/FaunaHealthApplication.java
@@ -0,0 +1,13 @@
+package com.faunahealth.web.faunaHealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaunaHealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaunaHealthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/faunahealth/web/faunaHealth/FaunaHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faunahealth.web.faunaHealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaunaHealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
